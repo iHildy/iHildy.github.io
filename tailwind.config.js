c25408fa05100190c418063e86f7b82f437d478a
iHildy <ian@jedi.net>
2020-10-17 15:22:07 -0500

    v2

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {
+      // screens: {
+      //   'sm': '640px',
+      //   // => @media (min-width: 640px) { ... }
+
+      //   'md': '780px',
+      //   // => @media (min-width: 768px) { ... }
+
+      //   'lg': '1034px',
+      //   // => @media (min-width: 1024px) { ... }
+
+      //   'xl': '1290px',
+      //   // => @media (min-width: 1280px) { ... }
+      // },
+      colors: {
+        // primary: "#",
+        // secondary: "#",
+        offwhite: "#f5f5f5",
+        offgray: "#202020",
+      }
+    }
+  },
+  variants: {
+    animation: ['responsive', 'motion-safe', 'motion-reduce']
+  },
+  plugins: []
+}

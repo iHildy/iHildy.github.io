207a3c155736ba26cc941897c7baf15daa987a15
iHildy <ian@jedi.net>
2020-12-15 22:19:56 -0600

    Struture setup

--- imswimmer/README.md
+++
@@ -1,2 +0,0 @@
-# imswimmer-responsive
-[Future Project] "Fork" of imswimmer.com to make it more responsive and look better using tailwind
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,12 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-    // defaultLineHeights: true,
-    // standardFontWeights: true
-  },
-  purge: [],
+  purge: [
+     './*.html',
+     './**/*.html',
+   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      // screens: {
-      //   'sm': '640px',
-      //   // => @media (min-width: 640px) { ... }
-
-      //   'md': '780px',
-      //   // => @media (min-width: 768px) { ... }
-
-      //   'lg': '1034px',
-      //   // => @media (min-width: 1024px) { ... }
-
-      //   'xl': '1290px',
-      //   // => @media (min-width: 1280px) { ... }
-      // },
-      colors: {
-        // primary: "#",
-        // secondary: "#",
-        offwhite: "#f5f5f5",
-        offgray: "#202020",
-        discord: "#7289DA",
-      }
-    }
+    extend: {},
   },
-  variants: {
-    animation: ['responsive', 'motion-safe', 'motion-reduce']
-  },
-  plugins: []
+  variants: {},
+  plugins: [],
 }

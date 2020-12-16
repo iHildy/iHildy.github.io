ba20fcd170196283762f8ca95f4eff65ed7e0df4
iHildy <ian@jedi.net>
2020-12-15 22:38:11 -0600

    added primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [
-     './*.html',
-     './**/*.html',
-   ],
+  // purge: [
+  //    './*.html',
+  //    './**/*.html',
+  //  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        offwhite: "#f5f5f5",
+        darkgray: "#32373b",
+
+        primary: {
+        '900': '#142C28',
+        '800': '#20453E',
+        '700': '#449194',
+        '600': '#398384',
+        '500': '#60B9A9',
+        '400': '#60B9B9',
+        '300': '#5BBDC0',
+        '200': '#7CCECE',
+        },
+        
+        gray: colors.trueGray,
+      },
+    },
   },
   variants: {},
   plugins: [],

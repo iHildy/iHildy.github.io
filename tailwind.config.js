59e0fa0a4875d17e6119ba85d78b9bcb4f274d18
iHildy <ian@jedi.net>
2021-01-26 11:11:17 -0600

    added templates back

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [
-     './*.html',
-     './**/*.html',
-   ],
+    "./*.html",
+    "./**/*.html",
+    "./**/**/*.html",
+    "./**/**/**/*.html",
+    "./**/**/**/**/*.html",
+    "./**/**/**/**/**/*.html",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -13,24 +17,24 @@ module.exports = {
         darkgray: "#32373b",
 
         primary: {
-        '900': '#142C28',
-        '800': '#20453E',
-        '700': '#449194',
-        '600': '#398384',
-        '500': '#60B9A9',
-        '400': '#60B9B9',
-        '300': '#5BBDC0',
-        '200': '#7CCECE',
+          900: "#142C28",
+          800: "#20453E",
+          700: "#449194",
+          600: "#398384",
+          500: "#60B9A9",
+          400: "#60B9B9",
+          300: "#5BBDC0",
+          200: "#7CCECE",
         },
-        
+
         gray: colors.trueGray,
       },
       fontFamily: {
-        'name': 'JetBrains Mono',
-        'para': 'SF Pro Display Regular'
-      }
+        name: "JetBrains Mono",
+        para: "SF Pro Display Regular",
+      },
     },
   },
   variants: {},
   plugins: [],
-}
+};

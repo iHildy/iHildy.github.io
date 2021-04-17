f911dd67d0bc73309dd24edd9947c4e934dde2d4
iHildy <ian@jedi.net>
2021-04-16 22:24:46 -0500

    maybe fixed errors

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Modal from "../utils/Modal";
 
 function HeroHome() {
-  const [videoModalOpen, setVideoModalOpen] = useState(false);
 
   return (
     <section className="relative">

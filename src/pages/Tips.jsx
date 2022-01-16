688796bb89d5ceeba09a54829fef9822c20531bb
iHildy <ian@jedi.net>
2022-01-15 23:13:27 -0600

    maybe fixed

--- src/pages/Tips.jsx
+++ src/pages/Tips.jsx
@@ -2,12 +2,8 @@ import React from "react";
 
 import Header from "../partials/Header";
 import HeroTips from "../partials/HeroTips";
-// import FeaturesBlocks from "../partials/FeaturesBlocks";
-// import Testimonials from "../partials/Testimonials";
-// import Newsletter from "../partials/Newsletter";
-// import Footer from "../partials/Footer";
 
-function Home() {
+function Tips() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       <Header />
@@ -21,4 +17,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Tips;

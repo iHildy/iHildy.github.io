dd8102401a3ea1047bc6c7917bc3e5f23612a8c1
iHildy <ian@jedi.net>
2025-08-28 08:09:57 -0500

    added mobild x axis padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Projects from "@/app/_components/projects";
 
 export default async function Home() {
   return (
-      <main className="flex min-h-screen flex-col mx-auto text-black m-10 max-w-3xl gap-4">
+      <main className="flex min-h-screen flex-col mx-auto text-black m-10 max-w-3xl gap-4 px-4 lg:px-0">
         <Hero />
         <WorkExperience />
         <Projects />
